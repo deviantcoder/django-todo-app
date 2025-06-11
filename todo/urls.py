3ea49407293fe5396527d32ddec3e2c82c2550d7
Andrew <andrewsafonovpost@gmail.com>
2025-06-11 19:39:37 +0300

    Added dynamic tasks creation + form errors handling

--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,8 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('tasks/list/', views.get_task_list, name='get_task_list'),
+    path('tasks/create/', views.create_task_view, name='create_task'),
     path('tasks/mark/completed/<str:id>/', views.MarkTaskCompletedView.as_view(), name='mark_task_completed'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.http import HttpResponse
 
 from .forms import TaskForm
 from .models import Task
@@ -16,20 +18,17 @@ class HomeView(generic.TemplateView):
             return redirect('todo:dashboard')
         
         return super().dispatch(request, *args, **kwargs)
-
-
-class DashboardView(LoginRequiredMixin, generic.TemplateView):
-    template_name = 'todo/dashboard.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task_form'] = TaskForm(user=self.request.user)
-        context['tasks'] = self.request.user.tasks.filter(is_completed=False, is_deleted=False).select_related('category')
-        context['completed_tasks'] = self.request.user.tasks.filter(is_completed=True).select_related('category')
-
-        return context
     
-    def post(self, request, *args, **kwargs):
+
+@login_required
+def dashboard_view(request):
+    context = {
+        'task_form': TaskForm(user=request.user),
+        'tasks': request.user.tasks.filter(is_completed=False, is_deleted=False).select_related('category'),
+        'completed_tasks': request.user.tasks.filter(is_completed=True).select_related('category'),
+    }
+
+    if request.method == 'POST':
         form = TaskForm(request.POST, user=request.user)
 
         if form.is_valid():
@@ -41,13 +40,36 @@ class DashboardView(LoginRequiredMixin, generic.TemplateView):
 
             return redirect('todo:dashboard')
         else:
-            context = self.get_context_data()
-            context['task_form'] = form
-
             for error in form.non_field_errors():
                 messages.warning(request, error)
+            context['task_form'] = form
+            return render(request, 'todo/dashboard.html', context)
 
-            return self.render_to_response(context)
+    return render(request, 'todo/dashboard.html', context)
+
+
+@login_required
+def get_task_list(request):
+    tasks = request.user.tasks.filter(is_completed=False, is_deleted=False).select_related('category')
+    return render(request, 'todo/partials/_task_list.html', {'tasks': tasks})
+
+
+@login_required
+def create_task_view(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST, user=request.user)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+
+            messages.success(request, 'Task created!')
+
+            return HttpResponse(status=204)
+
+        return render(request, 'todo/partials/_task_form.html', {'task_form': form})
+    return redirect('todo:dashboard')
 
 
 class MarkTaskCompletedView(LoginRequiredMixin, View):
